dd7cc0a2b32045a758a46ae502aa5d72ffb9f19f
Saraloal <saloal2004@gmail.com>
2025-10-30 08:38:35 +0100

    Add deploy configuration

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,3 @@
-import PhoenixIcon from '../assets/logos/Phoenix-logo.svg?raw';
-import N8nIcon from '../assets/logos/N8n-logo.svg?raw';
-import SupabaseIcon from '../assets/logos/Supabase-logo.svg?raw';
-import PosthogIcon from '../assets/logos/posthog-logo.svg?raw';
-import PythonIcon from '../assets/logos/Python-logo.svg?raw';
-import TailwindIcon from '../assets/logos/Tailwind-logo.svg?raw';
-
-
 export const projects = [
 	{
 		title: "Workflow Automation Orchestrator (n8n)",
